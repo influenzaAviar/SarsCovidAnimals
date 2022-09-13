bd68b0151a6c60bb1205e695e49cace9b6adce38
Carlos29Blanco <60890981+Carlos29Blanco@users.noreply.github.com>
2022-09-13 11:22:08 +0200

    timeSliderBrotes
    timeSliderBrotes

--- js/aviarScript.js
+++ js/aviarScript.js
@@ -58,7 +58,7 @@ require([
   };
   // Request feature layers and overwrite renderer 
   const featureLayerBrotes = new FeatureLayer({
-    url: "https://gis.inia.es/server/rest/services/Hosted/histAviar/FeatureServer",
+    url: "https://gis.inia.es/server/rest/services/Hosted/SARS_animals_OIE/FeatureServer",
     copyright: "Influenza Aviar",
     title: "Brotes",
     outFields: ['*'],
@@ -71,7 +71,7 @@ require([
       returnGeometry: true,
       fieldInfos: [
         {
-          fieldName: 'observationDate',
+          fieldName: 'reporting_date',
           format: {
             dateFormat: 'short-date'
           }
@@ -83,181 +83,20 @@ require([
 
   function getInfoBrotes(feature) {
     content = "<p>Número de casos: <b>{cases}</b> " +
-      "<ul><li>Location: {location}.</li>" +
-      "<li>Report date: {report_date}.</li>" +
-      "<li>Species: {species}.</li>" +
-      "<li>Serotype: {serotype}.</li>" +
+      "<ul><li>Location: {location_name}.</li>" +
+      "<li>Report date: {reporting_date}.</li>" +
+      "<li>Species: {species_resum}.</li>" +
+      "<li>Disease: {disease}.</li>" +
       "<li>More info: <a href='http://empres-i.fao.org/eipws3g/2/obd?idOutbreak={event_id}'> Enlace</a></li>";
 
     return content;
 
   }
 
-  var lineSymbolMigrations = new LineSymbol3D({
-    symbolLayers: [
-      new LineSymbol3DLayer({
-        material: { color: [237, 237, 237, 0.3] },
-        size: 0.1
-      })
-    ]
-  });
-
-
-  var rendererMigrations = new SimpleRenderer({
-
-    symbol: lineSymbolMigrations
-
-  });
-
-  const featureLayerRutas = new FeatureLayer({
-    url: "https://gis.inia.es/server/rest/services/Hosted/rutasUsaFinal/FeatureServer",
-    copyright: "CERBU | INIA-CSIC",
-    title: "Movements",
-    outFields: ["*"],
-    renderer: rendererMigrations,
-    popupTemplate: {
-      title: "Group: {Group_spec}" +
-        "<br>Total number of banding records: {Total}",
-      /* content: [
-          {
-              type: "fields",
-              fieldInfos: [
-                  {
-                      fieldName: "species",
-                      label: "Especie",
-                      visible: true
-                  },
-              {
-                  fieldName: "idAlerta",
-                  label: "Codigo",
-                  visible: true
-              },
-              ]
-          }
-      ] */
-    },
-    visible: false,
-    availableFields: true,
-
-  });
-
-
-
-  const rendererNuts = {
-    type: "simple",
-    symbol: {
-      type: "simple-fill",
-      color: [178, 220, 247, 0.03],
-      outline: {
-        color: [4, 178, 194],
-        width: 1
-      }
-    }
-  };
-
-  window.onload = function () {
-    document.getElementById("migrations").addEventListener("click", activarMigrations);
-
-    view.ui.add(migrations, "top-right");
-
-  }
-
-
-
-  function activarMigrations(feature) {
-    if (featureLayerRutas.visible === false) {
-      return featureLayerRutas.visible = true;
-    } else {
-      return featureLayerRutas.visible = false;
-    }
-
-  }
-
-  /// DEFINICIÓN DE LOS NUTS
-
-  const featureLayerNuts = new FeatureLayer({
-    url: "https://gis.inia.es/server/rest/services/Hosted/nuts_Simplify/FeatureServer",
-    copyright: "CERBU | INIA-CSIC",
-    title: "Nuts",
-    outFields: ['*'],
-    visible: true,
-    renderer: rendererNuts,
-    supportsQuery: true,
-    popupTemplate: {
-      title: "Admin: {ADMIN_NAME}," +
-        "<br>Country: {CNTRY_NAME}"/*  +
-            "<br>Group: {Group_spec_1}</br>" */,
-      content: getInfoComarcas,
-      visible: false,
-      returnGeometry: true,
-    },
-
-  });
-
-
-
-  /// ESTA FUNCIÓN PROGRAMA EL POPUPTEMPLATE
-  function getInfoComarcas(feature) {
-  
-    /* view.graphics.removeAll() */
-
-    var graphic, attributes;
-
-    graphic = feature.graphic;
-    attributes = graphic.attributes;
-    /* console.log("Atributes:" + attributes) */
-
-
-    var urlRutas = 'https://raw.githubusercontent.com/influenzaAviar/applicacionWeb3D/main/GeoJSON/rutasUsa.geojson';
-    // Se inicia la peticion ajax a la url ruta
-    
-    var request = new XMLHttpRequest();
-    request.open("GET", urlRutas, false); // false for synchronous request
-    request.send(null);
-    let rutas = JSON.parse(request.responseText)
-    console.log('obj ruta', rutas)
-
-    for (let index = 0; index < rutas.features.length; index++) {
-      const element = rutas.features[index];
-      console.log('element', element)
-      if (element.properties.FIPS_ADMIN_Recov == attributes.fips_admin || element.properties.FIPS_ADMIN_banding == attributes.fips_admin ) {
-        var polyline = {
-          type: "polyline", // new Polyline()
-          paths: element.geometry.coordinates
-        };
-        var lineSymbol = {
-          type: "simple-line", // new SimpleLineSymbol()
-          color: [51, 200, 200/* , 0.9 */], // RGB color values as an array
-          width: 1
-        };
-        var polylineGraphic = new Graphic({
-          geometry: polyline, // Add the geometry created in step 4
-          symbol: lineSymbol, // Add the symbol created in step 5
-          popupTemplate: {
-            title: "Group_spec: " + element.properties.Group_spec +
-              "<br>Total: " + element.properties.Total/*  +
-                  "<br>Group: {Group_spec_1}</br>" */,
-            content: getInfoComarcas,
-            visible: false,
-            returnGeometry: true,
-          },
-        });
-        view.graphics.add(polylineGraphic);
-      }
-    }
-
-    view.on("hold", function (e) {
-      view.graphics.removeAll(polylineGraphic);
-      console.log("Remove")
-    })
-
-    
-  }
-
   // Create the Map
   const map = new Map({
     basemap: "hybrid",
-    layers: [featureLayerBrotes, featureLayerNuts, featureLayerRutas]
+    layers: [featureLayerBrotes]
   });
 
   // Create the SceneView and set initial camera
@@ -266,8 +105,8 @@ require([
     map: map,
     camera: {
       position: {
-        latitude: 20.00000,
-        longitude: -105.00000,
+        latitude: 25.00000,
+        longitude: 20.00000,
         z: 10000000
       },
       tilt: 11.5,
@@ -387,14 +226,14 @@ require([
     layerViewBrotes = lv;
 
     // hora de inicio del control deslizante de tiempo
-    const startBrotes = new Date();
-    startBrotes.setHours(0, 0, 0, 0);
+    const startBrotes = new Date(2020, 0, 1);
+    /* startBrotes.setHours(0, 0, 0, 0);
     startBrotes.setDate(startBrotes.getDate());
-    startBrotes.setDate(startBrotes.getDate() - 455);
+    startBrotes.setDate(startBrotes.getDate() - 1095); */
 
-    const LastMonday = new Date();
-    LastMonday.setHours(0, 0, 0, 0);
-    LastMonday.setDate(LastMonday.getDate());
+    const LastMonday = new Date(2022, 7, 10);
+    /* LastMonday.setHours(0, 0, 0, 0);
+    LastMonday.setDate(LastMonday.getDate()); */
 
     // set time slider's full extent to
     // until end date of layer's fullTimeExtent
@@ -403,7 +242,7 @@ require([
       end: LastMonday
     };
     const endBrotes = new Date(LastMonday);
-    endBrotes.setDate(endBrotes.getDate() - 91);
+    endBrotes.setDate(endBrotes.getDate() - 165);
 
     timeSliderBrotes.values = [endBrotes, LastMonday];
   });
